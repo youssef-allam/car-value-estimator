7fd095557e50cc51e8327c19c94a9512d498d715
--get <youssefsamirallam@gmail.com>
2026-08-24 03:00:17 +0300

    userSignup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    constructor(private readonly userService: UsersService) {}
+
+    @Post('signup')
+    createUser(@Body() createUserDto: CreateUserDto) {
+        console.log('Received user data:', createUserDto);
+        return this.userService.createUser(createUserDto);
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './users.entity.ts/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private readonly repo: Repository<User>) {}
+
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+        const user = await this.repo.create(createUserDto);
+        return this.repo.save(user);
+    }
+}
