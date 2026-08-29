753b90dde0f35ec35f52508e9dcb599b47b46b46
--get <youssefsamirallam@gmail.com>
2026-08-29 06:38:28 +0300

    Unit testing SignIn in users Controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -19,7 +19,13 @@ describe('UsersController', () => {
         } as User);
       },
       find: (email: string) => {
-        return Promise.resolve([ { id: Math.floor(Math.random() * 99999), email, password:'a' } as User]);
+        return Promise.resolve([
+          {
+            id: Math.floor(Math.random() * 99999),
+            email,
+            password: 'a',
+          } as User,
+        ]);
       },
       // remove: () => {},
       // update: () => {},
@@ -28,21 +34,23 @@ describe('UsersController', () => {
 
     faskeAuthService = {
       // signup: () => {},
-      // signIn: () => {},
+      signIn: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers:[
+      providers: [
         {
-          provide:UsersService,
-          useValue: fakeUsersService
+          provide: UsersService,
+          useValue: fakeUsersService,
         },
         {
           provide: AuthService,
-          useValue: faskeAuthService
-        }
-      ]
+          useValue: faskeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -52,23 +60,35 @@ describe('UsersController', () => {
     expect(controller).toBeDefined();
   });
 
-  it("gets User by id",async ()=>{
+  it('gets User by id', async () => {
     const user = await controller.getUser(1);
 
     expect(user).toBeDefined();
-  })
+  });
 
-  it("throws an err if user Not found",async ()=>{
-    fakeUsersService.findOne = (id:number) => null;
+  it('throws an err if user Not found', async () => {
+    fakeUsersService.findOne = (id: number) => null;
 
     await expect(controller.getUser(1)).rejects.toThrow();
-  })
+  });
 
-  it("getUsers returns all users with current email",async ()=>{
-    const user = await controller.getUsers("g@gmail.com");
+  it('getUsers returns all users with current email', async () => {
+    const user = await controller.getUsers('g@gmail.com');
 
     expect(user.length).toEqual(1);
-    expect(user[0].email).toEqual("g@gmail.com");
-  })
+    expect(user[0].email).toEqual('g@gmail.com');
+  });
 
+  it("signIn sets session ID correctly",async ()=>{
+    const session = {
+      userId: 10,
+    }
+    const body = {id:1 , email: "a@gmail.com" , password: "a"};
+
+    const user = await controller.signin(body , session);
+
+    expect(session.userId).toEqual(1);
+    expect(user).toBeDefined();
+
+  })
 });
