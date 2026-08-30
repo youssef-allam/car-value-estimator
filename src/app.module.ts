8ddcde24e3fe6f2f596445ab3ccaace6abc80cd2
--get <youssefsamirallam@gmail.com>
2026-08-30 07:23:42 +0300

    setup env in appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/users.entity.ts/user.entity';
 import { Report } from './reports/reports.entity.ts/report.entity';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject:[ConfigService],
+      useFactory:(config: ConfigService) =>{
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities:[User,Report]
+        }
+      }
     }),
     UsersModule,
     ReportsModule,
