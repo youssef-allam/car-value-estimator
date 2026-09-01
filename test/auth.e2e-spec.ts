4a52860f9f19be7511f57a689772716fbe54676b
--get <youssefsamirallam@gmail.com>
2026-09-01 20:35:26 +0300

    Testing WhoAmI route after signup

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -17,19 +17,36 @@ describe('Authentication system', () => {
   });
 
   it('handles signup request', () => {
-    const email = "ds@gmail.com";
+    const email = 'ds@gmail.com';
 
     return request(app.getHttpServer())
       .post('/auth/signup')
-      .send({email , password: "123"})
+      .send({ email, password: '123' })
       .expect(201)
-      .then((res)=> {
-        const {id , email} = res.body;
+      .then((res) => {
+        const { id, email } = res.body;
 
-        expect(id).toBeDefined()
-        expect(email).toEqual(email)
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
 
-      })
+  it('signup as a new User and then get the currently logedin user', async () => {
+    const email = 'example@gmail.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '123' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoAmI')
+      .set('Cookie', cookie ? cookie.join(';') : '')
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 
   afterEach(async () => {
