2f37980abf8ffa04f6a3d1d0c80840e89c6dc652
--get <youssefsamirallam@gmail.com>
2026-09-02 02:05:15 +0300

    adding toBeDefoned test for reports Controller

--- src/reports/reports.controller.spec.ts
+++ src/reports/reports.controller.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReportsController } from './reports.controller';
+import { ReportsService } from './reports.service';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Report } from './reports.entity.ts/report.entity';
 
 describe('ReportsController', () => {
   let controller: ReportsController;
+  let fakeReportsService: Partial<ReportsService>;
 
   beforeEach(async () => {
+    fakeReportsService = {
+      create(body: CreateReportDto) {
+        return Promise.resolve(body as Report);
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReportsController],
+      providers: [
+        {
+          provide: ReportsService,
+          useValue: fakeReportsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ReportsController>(ReportsController);
@@ -15,4 +31,6 @@ describe('ReportsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+
 });
