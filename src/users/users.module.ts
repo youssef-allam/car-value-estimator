ee71d52b93a09fa31cfb302863ac45d1714fe837
--get <youssefsamirallam@gmail.com>
2026-09-03 05:16:56 +0300

    Remove unused currentUserInterceptor from UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,8 +4,6 @@ import { UsersService } from './users.service';
 import { User } from './users.entity.ts/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
-import { currentUserInterceptor } from './interceptors/current-user.interceptor';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 
 @Module({
@@ -14,11 +12,6 @@ import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
   providers: [
     UsersService,
     AuthService,
-    currentUserInterceptor,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: currentUserInterceptor,
-    },
   ],
 })
 export class UsersModule {
