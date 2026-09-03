64d8d61b8a49cd44e0b9739c4a87794b3c5d7d58
--get <youssefsamirallam@gmail.com>
2026-09-03 05:15:34 +0300

    current-user.middleware

---
+++ src/users/middlewares/current-user.middleware.ts
@@ -0,0 +1,21 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction , Request , Response } from "express";
+import { UsersService } from "../users.service";
+
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+    constructor(private readonly userService: UsersService) {}
+
+    async use(req: Request, res: Response, next: NextFunction) {
+        const { userId } = req.session || {};
+
+        if (userId) {
+            const user = await this.userService.findOne(userId);
+            // @ts-ignore
+            req.currentUser = user;
+        }
+
+        next();
+    }
+}
